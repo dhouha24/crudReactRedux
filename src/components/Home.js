799841ec9957ae848af0ba60a6ae5435d748d425
Dhouha <dhouha.lahbib@gmail.com>
2022-10-11 11:40:54 +0100

    list contacts

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const Home = () => {
+  const contacts = useSelector((state) => state);
+
   return (
     <div className="container">
       <div className="row">
@@ -10,8 +13,40 @@ const Home = () => {
             Add Contact
           </Link>
         </div>
-        <div className="col-md-6 mx-auto">
-          <h1>Bonjour</h1>
+        <div className="col-md-10 mx-auto">
+          <table className="table table-hover">
+            <thead className="text-white bg-dark text-center">
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Number</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {contacts.map((contact, id) => (
+                <tr key={id}>
+                  <td>{id + 1}</td>
+                  <td>{contact.name}</td>
+                  <td>{contact.email}</td>
+                  <td>{contact.number}</td>
+                  <td>
+                    <Link
+                      to={`/edit/${contact.id}`}
+                      className="btn btn-primary btn-small me-2"
+                    >
+                      Edit
+                    </Link>
+
+                    <button type="button" className="btn btn-danger btn-small">
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
